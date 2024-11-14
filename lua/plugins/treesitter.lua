b6e5a15ae2bb4265f335777500342c6cd0667686
Popoch39 <pocheron.louis@gmail.com>
2024-11-14 23:10:17 +0100

    modified treeSitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,27 +1,47 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    tag = "v0.9.1",
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    build = ":MasonUpdate",
     opts = {
       ensure_installed = {
-        "javascript",
-        "typescript",
-        "css",
-        "gitignore",
-        "graphql",
-        "http",
-        "json",
-        "scss",
-        "sql",
-        "vim",
-        "lua",
-        "php",
-      },
-      query_linter = {
-        enable = true,
-        use_virtual_text = true,
-        lint_events = { "BufWrite", "CursorHold" },
+        "typescript-language-server",
+        "css-lsp",
+        "prettier",
       },
     },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "javascript",
+          "typescript",
+          "css",
+          "gitignore",
+          "graphql",
+          "http",
+          "json",
+          "scss",
+          "sql",
+          "vim",
+          "lua",
+          "php",
+        },
+        highlight = { enable = true },
+        auto_install = true,
+        query_linter = {
+          enable = true,
+          use_virtual_text = true,
+          lint_events = { "BufWrite", "CursorHold" },
+        },
+      })
+    end,
   },
 }
