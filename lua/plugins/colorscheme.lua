df5f984d6aa453ecfab1b19c7e04ea3c2b73a28c
Louis Pocheron <louis.pocheron@davikingcode.com>
2025-01-19 22:24:31 +0100

    feat: lazy git

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,20 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 10000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 10000,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+        term_colors = true,
+        integrations = {
+          cmp = true,
+          treesitter = true,
+          telescope = true,
+        },
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,20 @@
+return {
+  "kdheepak/lazygit.nvim",
+  lazy = true,
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { silent = true, noremap = true, desc = "Open LazyGit" })
+  end,
+}
